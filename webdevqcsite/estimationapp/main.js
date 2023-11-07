9ad61face333107a9d8405e105456f4efa82c7de
webdevqcc <150162933+webdevqcc@users.noreply.github.com>
2023-11-07 10:10:19 -0500

    Add files via upload

---
+++ webdevqcsite/estimationapp/main.js
@@ -0,0 +1,10 @@
+
+// Options object ( not optional need to at least past empty object), Vue.createApp({})
+const app = Vue.createApp({
+  data(){
+    return{
+    test:'test'
+    }
+    
+  }
+})
